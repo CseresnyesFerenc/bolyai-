c10e262949c03342030b5a37a0a568b68b502ed0
cseresnyesfe <cseresnyes.ferenc.tech2023c@bolyaimovar.com>
2024-11-13 09:41:07 +0100

    24/11/13

---
+++ új/grade calculator.py
@@ -0,0 +1,9 @@
+matek=[]
+magyar=[]
+tori=[]
+jegyek=[]
+jegy=None
+jegy = int(input("kérem adjon meg egy jegyet: "))
+while jegy!=0:
+    jegy.append[jegy]
+    jegy = int(input("kérem adjon meg egy jegyet: "))
