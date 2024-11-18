44d70e0fd77fecbea8a4b9e073162cb8e6232ac3
cseresnyesfe <cseresnyes.ferenc.tech2023c@bolyaimovar.com>
2024-11-18 14:07:23 +0100

    24/11/13

--- új/grade calculator.py
+++ új/grade calculator.py
@@ -1,9 +1,31 @@
-matek=[]
-magyar=[]
-tori=[]
-jegyek=[]
-jegy=None
-jegy = int(input("kérem adjon meg egy jegyet: "))
-while jegy!=0:
-    jegy.append[jegy]
-    jegy = int(input("kérem adjon meg egy jegyet: "))
+def beker(szoveg,szam):
+    jegyek = []
+    jegy = int(input("Kérem adja meg a "+szoveg+" jegyet:"))
+    while jegy != 0:
+        jegyek.append(jegy)
+        jegy = int(input("Kérem adja megy a "+szoveg+" jegyet (1-5): "))
+    return atlag(jegyek)
+
+def atlag(lista):
+    hossz = len(lista)
+    vissza = 0
+    for i in range(hossz):
+        vissza = vissza + lista[i]
+
+    return round(vissza/hossz ,1)
+
+
+#******************************************************************************************************
+matek = 0
+tori = []
+magyar = []
+
+matek = beker("Magyar", 2)
+
+# tori = beker("Tori")
+# magyar = beker("Magyar")
+print("Matek:", matek)
+# print("Tori:", tori)
+# print("Magyar:", magyar)
+
+
---
+++ új/szám ellenőrző.py
@@ -0,0 +1,7 @@
+paros_e=None
+while paros_e!=0:
+    paros_e=int(input("Kérem adjon meg egy számot: "))
+if paros_e%2==0:
+    print("A szám Páros")
+if paros_e%2!=0:
+    print("A Szám Páratlan")
