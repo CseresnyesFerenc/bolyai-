8394dae4e3606a05c2a15a22d4bd83a4c239f34a
cseresnyesfe <cseresnyes.ferenc.tech2023c@bolyaimovar.com>
2024-11-27 09:39:11 +0100

    Update Házi2024.11.24.py

--- új/2024.11.25v2.py
+++ új/2024.11.25v2.py
@@ -1,7 +1,19 @@
 print("1.feladat")
 max=None
-lista1=[-14,23,1,-48,-43,28,-77,-33,-95,38,-9,98,-61,58,21,87,41,-65,-22,-20,-56,-75,80,-77,100,67,22,8,-78,-6]
+lista1=[14,23,1,48,43,28,77,33,95,38,9,98,61,58,21,87,41,65,22,20,56,75,80,77,100,67,22,8,78,6]
+negative=False
+osszeg=None
 print(len(lista1),"Darab elem van")
-if lista1[i]<0:
-    
+for i in range(len(lista1)):
+    if lista1[i]<0:
+        negative=True
+        break
 
+if negative:
+    print("Van negatív")
+else:
+    print("nincs negatív")
+for i in range(len(lista1)):
+    if lista1[i]%2==0:
+        count=len(lista1)
+print("páros számok:", count)
