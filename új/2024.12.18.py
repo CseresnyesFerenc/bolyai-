980f090f56710259ffa7549cc1d07b6a2048fc0e
CseresnyesFerenc <cseresnyes.ferenc.tech2023c@bolyaimovar.com>
2024-12-15 18:33:11 +0100

    2024.12.18

---
+++ új/2024.12.18.py
@@ -0,0 +1,16 @@
+def Mysplit(szoveg): #"valami_másik_egy"
+    szo=""
+    lista=[]
+    i=None
+while i <len(szoveg):
+    while betu[i]!="":
+        lista.append(szo)
+print(Mysplit("valami"))
+def Mysplit(szoveg): #"valami_másik_egy"
+    szo=""
+    lista=[]
+    i=None
+while i <len(szoveg):
+    while betu[i]!="":
+        lista.append(szo)
+print(Mysplit("valami"))
